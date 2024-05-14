10b6c114653a15bed5cf53a68621d9e5f105025e
Roton02 <rph645102@gmail.com>
2024-05-14 09:18:51 +0600

    implement jwt

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://a11-kynus-foodie.web.app",
+      "https://a11-kynus-foodie.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// MiddleWare
+const logger = (req, res, next) => {
+  console.log(req.hostname);
+  next();
+};
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  // console.log(req.cookies);
+  console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "Authorization access not allow" });
+  }
+  jwt.verify(token, process.env.SECRETE_ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "UnAuthorized Access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.mi2xoxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,10 +61,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //json web token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRETE_ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+    app.post("/loggout", async (req, res) => {
+      const user = req.body;
+      res.clearCookie("token", { maxAge: 0 }).send({ logoutSuccess: true });
+    });
+
     const FeaturedCollection = client
       .db("FoodPicky")
       .collection("FeaturedProduct");
-    app.get("/featured", async (req, res) => {
+    app.get("/featured",  async (req, res) => {
+      // console.log(req.user);
+     
       let query = {};
       if (req.query?.email) {
         query = { "donator.email": req.query.email };
@@ -58,14 +114,22 @@ async function run() {
       const cursor = await FeaturedCollection.find(query).toArray();
       res.send(cursor);
     });
-
     app.get("/featured/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await FeaturedCollection.findOne(query);
       res.send(result);
     });
-
+    app.get('/featured/:email' ,logger,verifyToken, async(req,res)=>{
+      if (req.params?.email !== req.user.email) {
+        return res.status(403).send({ message: "Forbidden" });
+      }
+      const email = req.params?.email;
+      const query = { "donator.email": email}
+      const cursor = await FeaturedCollection.find(query).toArray();
+      res.send(cursor);
+    })
+    
     app.post("/addFood", async (req, res) => {
       const foodData = req.body;
       const result = await FeaturedCollection.insertOne(foodData);
@@ -111,7 +175,11 @@ async function run() {
       );
       res.send(result);
     });
-    app.get("/requested/:email", async (req, res) => {
+    app.get("/requested/:email",logger,verifyToken, async (req, res) => {
+      console.log(req.user);
+      if (req.params?.email !== req.user.email) {
+        return res.status(403).send({ message: "Forbidden" });
+      }
       const mail = req.params.email;
       const query = { requsterEmail: mail };
       const result = await FeaturedCollection.find(query).toArray();
