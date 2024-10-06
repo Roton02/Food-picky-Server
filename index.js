3743c7d76a46f69b6829cf565583205e8a50b8cc
Roton02 <rph645102@gmail.com>
2024-10-06 23:23:12 +0600

    new response sent

--- index.js
+++ index.js
@@ -273,6 +273,7 @@ async function run() {
         updateData
       );
       res.send(result);
+      // res.send
     })
     app.patch('/admin/updateStockover', async (req, res) => {
       const id = req.body.id;
@@ -290,7 +291,7 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(" successfully connected to MongoDB!");
+    console.log(" successfully connected to MongoDB! asa");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
