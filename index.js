74b475bc39ceaffedb330c8136c64beeca5028f2
Roton02 <rph645102@gmail.com>
2024-10-18 13:14:35 +0600

    change the some ssection and deploy in firebase if  need any update then reopened the project

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     //json web token
     app.post("/jwt", async (req, res) => {
@@ -327,8 +327,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(" successfully connected to MongoDB! asa");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(" successfully connected to MongoDB! asa");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
